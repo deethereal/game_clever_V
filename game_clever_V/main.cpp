90f40241684a373ee56c74881b37251f8e6c074b
denis <mistrny@gmail.com>
2020-11-07 15:08:07 +0300

    Initial Commit

---
+++ game_clever_V/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game_clever_V
+//
+//  Created by denis  on 07.11.2020.
+//  Copyright © 2020 denis . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
